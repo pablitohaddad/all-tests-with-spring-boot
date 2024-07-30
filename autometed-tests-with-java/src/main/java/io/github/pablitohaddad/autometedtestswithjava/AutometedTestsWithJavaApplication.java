e135e785e4781f30f94a86e7c7d0fa001ed05921
pablitohaddad <pablohaddad73@gmail.com>
2024-07-30 10:08:19 -0400

    Firsts Steps With Spring Boot tests

---
+++ autometed-tests-with-java/src/main/java/io/github/pablitohaddad/autometedtestswithjava/AutometedTestsWithJavaApplication.java
@@ -0,0 +1,13 @@
+package io.github.pablitohaddad.autometedtestswithjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutometedTestsWithJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutometedTestsWithJavaApplication.class, args);
+	}
+
+}
---
+++ autometed-tests-with-java/src/test/java/io/github/pablitohaddad/autometedtestswithjava/AutometedTestsWithJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pablitohaddad.autometedtestswithjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutometedTestsWithJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
